73f716add8fb25bb12ca053e5ba263705d5db4ad
aysel <aydamirovaaysel7@gmail.com>
2021-11-08 15:21:27 +0400

    Добавьте файлы проекта.

---
+++ Extention/ExtentionMethods.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Extention
+{
+    static class ExtentionMethods
+    {
+        public static bool IsOdd(this int num1)
+        {
+            if (num1 % 2 != 0)
+            {
+                return true;
+            }
+            return false;
+        }
+        public static bool IsEven(this int num2)
+        {
+            if (num2 % 2 == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+        public static bool IsContainsDigit(this string text1)
+        {
+            for (int i = 0; i < text1.Length; i++)
+            {
+                if (char.IsDigit(text1[i]))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public static string ToCapitalize(this string text2)
+        {
+            return char.ToUpper(text2[0]) + text2.Substring(1).ToLower();
+        }
+        public static int[] GetValueIndexes(this string text3, char digit)
+        {
+            int count = 0;
+            for (int i = 0; i < text3.Length; i++)
+            {
+                if (text3[i] == digit)
+                {
+                    count++;
+                }
+            }
+
+            int[] arrayint = new int[count];
+            int index = 0;
+            for (int i = 0; i < text3.Length; i++)
+            {
+                if (text3[i] == digit)
+                {
+                    arrayint[index] = i;
+                    index++;
+                }
+            }
+            return arrayint;
+        }
+    }
+}
+}
---
+++ Extention/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Extention
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+                #region 
+                int reqem;
+                do
+                {
+                    Console.WriteLine("Reqem daxil edin");
+                    reqem = Convert.ToInt32(Console.ReadLine());
+                }
+                while (reqem == 0);
+                Console.WriteLine($"Tekdir? - {reqem.IsOdd()}");
+
+                int reqem2;
+                do
+                {
+                    Console.WriteLine("Reqem daxil edin");
+                    reqem2 = Convert.ToInt32(Console.ReadLine());
+                }
+                while (reqem2 == 0);
+                Console.WriteLine($"Cutdur? - {reqem2.IsEven()}");
+
+                string text1;
+                do
+                {
+                    Console.WriteLine("Metn daxil edin");
+                    text1 = Console.ReadLine();
+                }
+                while (string.IsNullOrWhiteSpace(text1));
+                Console.WriteLine($"Metn daxilinde reqem varmi?: {text1.IsContainsDigit()}");
+
+                string text2;
+                do
+                {
+                    Console.WriteLine("Metn daxil edin");
+                    text2 = Console.ReadLine();
+                }
+                while (string.IsNullOrWhiteSpace(text2));
+                Console.WriteLine(text2.ToCapitalize());
+
+                string text3;
+                do
+                {
+                    Console.WriteLine("Metn daxil edin");
+                    text3 = Console.ReadLine();
+                }
+                while (string.IsNullOrWhiteSpace(text3));
+                char digit = 'u';
+                int[] arrayindex = text3.GetValueIndexes(digit);
+                foreach (var item in arrayindex)
+                {
+                    Console.WriteLine(item);
+                }
+                #endregion
+            }
+        }
+    }
